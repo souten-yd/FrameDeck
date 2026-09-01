4a665623a98dd70be93a941f2b5ad0a132ff6a45
souten <k.kenshirou@gmail.com>
2026-09-01 20:32:30 +0900

    Hide docExtractor folders case-insensitively

--- framedeck/core/path_filters.py
+++ framedeck/core/path_filters.py
@@ -6,4 +6,4 @@ DOCEXTRACTOR_INTERNAL_PREFIX = ".docExtractor-"
 
 def is_internal_directory_name(name: str) -> bool:
     """漫画・動画UIや読書シーケンスに出さない内部ディレクトリか判定する。"""
-    return name.startswith(DOCEXTRACTOR_INTERNAL_PREFIX)
+    return name.casefold().startswith(DOCEXTRACTOR_INTERNAL_PREFIX.casefold())
